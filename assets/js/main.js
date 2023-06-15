b92687ba37c43f5006078bc1ab396d6bc3a2a43c
Ivny Metzker <ivnymetzker@gmail.com>
2023-06-14 21:12:24 -0300

    Mobile
    Corrigindo responsividade geral do site e adicionando efeito ao scrollar

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* MENU */
+
+function menuMobile() {
+    const nav = document.querySelector(".nav-bar");
+    nav.classList.toggle("active");
+  }
+
+  /* EFFECT TRANSITION */
+
+  const target = document.querySelectorAll("[data-animated]");
+
+function animation() {
+  const animationClass = "animate";
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    }
+  });
+}
+
+if (target.length) {
+  window.addEventListener("scroll", () => {
+    animation();
+  });
+}
+
+  
